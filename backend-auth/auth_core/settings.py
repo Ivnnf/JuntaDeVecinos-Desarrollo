c8208dd0c5dc6bf20f07fc9c8043e4e70a9c07bf
Ivan Tamayo <iftamayoh@gmail.com>
2026-08-27 23:00:05 -0400

    Configura MySQL y agrega modelo de roles

--- backend-auth/auth_core/settings.py
+++ backend-auth/auth_core/settings.py
@@ -13,9 +13,13 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 import os
 
+
+from dotenv import load_dotenv
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
@@ -79,9 +83,16 @@ WSGI_APPLICATION = 'auth_core.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
@@ -157,3 +168,5 @@ REST_FRAMEWORK = {
         "auth_api.authentication.CookieJWTAuthentication",
     ],
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- backend-auth/profiles/models.py
+++ backend-auth/profiles/models.py
@@ -68,3 +68,12 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+class Rol(models.Model):
+    nombre = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.nombre    
